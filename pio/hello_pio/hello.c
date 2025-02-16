447e095cc51dab7259c4da52068fe9b01763f5a0
Tom Foltz <thomasfoltz@yahoo.com>
2025-02-16 18:02:15 -0500

    new

--- pio/hello_pio/hello.c
+++ pio/hello_pio/hello.c
@@ -1,60 +1,88 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include <stdio.h>
-
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
-// Our assembled program:
-#include "hello.pio.h"
+#include "hardware/dma.h"
+#include "hardware/clocks.h"
+#include "pio_i2s.h 
 
-// This example uses the default led pin
-// You can change this by defining HELLO_PIO_LED_PIN to use a different gpio
-#if !defined HELLO_PIO_LED_PIN && defined PICO_DEFAULT_LED_PIN
-#define HELLO_PIO_LED_PIN PICO_DEFAULT_LED_PIN
-#endif
-
-// Check the pin is compatible with the platform
-#if HELLO_PIO_LED_PIN >= NUM_BANK0_GPIOS
-#error Attempting to use a pin>=32 on a platform that does not support it
-#endif
+// Define the PIO program for reading and writing data
+static inline void pio_read_write_program(pio_program_t *program) {
+    program->instructions[0] = pio_encode_wait_gpio(0, 8); // Wait for negative edge on pin 8 (clock)
+    program->instructions[1] = pio_encode_in(pio_pins, 8);    // Read 8 input pins
+    program->instructions[2] = pio_encode_out(pio_pins, 8);   // Output 8 bits
+    program->instructions[3] = pio_encode_jmp_rel(-3);      // Loop back
+    program->length = 4;
+    program->origin = -1;
+}
 
 int main() {
-#ifndef HELLO_PIO_LED_PIN
-#warning pio/hello_pio example requires a board with a regular LED
-#else
-    PIO pio;
-    uint sm;
-    uint offset;
+    stdio_init_all();
 
-    setup_default_uart();
+    // Choose PIO instance and pins
+    PIO pio = pio1;
+    int sm = 0;
+    uint data_pin_start = 0;
+    uint clock_pin = 8;
+    uint dma_channel = 0;
 
-    // This will find a free pio and state machine for our program and load it for us
-    // We use pio_claim_free_sm_and_add_program_for_gpio_range so we can address gpios >= 32 if needed and supported by the hardware
-    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&hello_program, &pio, &sm, &offset, HELLO_PIO_LED_PIN, 1, true);
-    hard_assert(success);
+    // Load PIO program
+    pio_program_t program;
+    pio_read_write_program(&program);
+    uint offset = pio_add_program(pio, &program);
 
-    // Configure it to run our program, and start it, using the
-    // helper function we included in our .pio file.
-    printf("Using gpio %d\n", HELLO_PIO_LED_PIN);
-    hello_program_init(pio, sm, offset, HELLO_PIO_LED_PIN);
+    // Configure PIO state machine
+    pio_sm_config c = pio_get_default_sm_config();
+    sm_config_set_in_pins(&c, data_pin_start);
+    sm_config_set_out_pins(&c, data_pin_start, 8);
+    sm_config_set_set_pins(&c, data_pin_start, 8);
+    sm_config_set_clkdiv(&c, 1);
+    sm_config_set_wrap(&c, offset, offset + program.length);
+    sm_config_set_in_shift(&c, false, false, 0);
+    sm_config_set_out_shift(&c, false, false, 8);
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
+    sm_config_set_sideset_pins(&c, clock_pin);
 
-    // The state machine is now running. Any value we push to its TX FIFO will
-    // appear on the LED pin.
-    // press a key to exit
-    while (getchar_timeout_us(0) == PICO_ERROR_TIMEOUT) {
-        // Blink
-        pio_sm_put_blocking(pio, sm, 1);
-        sleep_ms(500);
-        // Blonk
-        pio_sm_put_blocking(pio, sm, 0);
-        sleep_ms(500);
+    // Enable input and output on pins
+    for (uint i = 0; i < 8; i++) {
+        pio_gpio_init(pio, data_pin_start + i);
+        gpio_set_dir(data_pin_start + i, GPIO_IN);
+        pio_sm_set_pindirs_with_mask(pio, sm, (1u << (data_pin_start + i)), 0);
     }
 
-    // This will free resources and unload our program
-    pio_remove_program_and_unclaim_sm(&hello_program, pio, sm, offset);
-#endif
+    for (uint i = 0; i < 8; i++) {
+        pio_gpio_init(pio, data_pin_start + i);
+        gpio_set_dir(data_pin_start + i, GPIO_OUT);
+        pio_sm_set_pindirs_with_mask(pio, sm, (1u << (data_pin_start + i)), (1u << (data_pin_start + i)));
+    }
+
+    // Configure clock pin
+    pio_gpio_init(pio, clock_pin);
+    gpio_set_dir(clock_pin, GPIO_IN);
+
+    // Initialize and enable PIO state machine
+    pio_sm_init(pio, sm, offset, &c);
+    pio_sm_set_enabled(pio, sm, true);
+
+    // DMA setup
+    dma_channel_config dma_config = dma_channel_get_default_config(dma_channel);
+
+    // Configure DMA channel
+    channel_config_set_transfer_data_size(&dma_config, DMA_SIZE_32);
+    channel_config_set_read_increment(&dma_config, false);
+    channel_config_set_write_increment(&dma_config, false);
+    channel_config_set_dreq(&dma_config, pio_get_dreq(pio, sm, false));
+
+    // Start DMA transfer
+    dma_channel_configure(
+        dma_channel,
+        &dma_config,
+        &pio->txf[sm], // Write address
+        &pio->rxf[sm], // Read address
+        -1, // Number of transfers (infinite)
+        true // Start immediately
+    );
+    
+    while (true) {
+        tight_loop_contents();
+    }
+    return 0;
 }
